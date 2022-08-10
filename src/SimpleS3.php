b5e8a83802e1e64a9a0345ee31c2aa51285bd4a0
Matthieu Napoli <matthieu@mnapoli.fr>
2022-08-10 18:42:47 +0200

    Add support for headers and more

--- src/SimpleS3.php
+++ src/SimpleS3.php
@@ -5,19 +5,31 @@ namespace Mnapoli\SimpleS3;
 use DOMDocument;
 use RuntimeException;
 
+/**
+ * @phpstan-type Response array{ int, string, array<string, string> }
+ */
 class SimpleS3
 {
-    public static $connect_timeout = 10;
-    public static $timeout = 60;
+    private const TIMEOUT_IN_SECONDS = 2;
     public static $auto_retry_delays = [0, 1, 2]; // set to empty array to disable auto-retrying
 
     private string $accessKeyId;
     private string $secretKey;
-    private string $sessionToken;
+    private ?string $sessionToken;
     private string $region;
     private ?string $endpoint;
 
-    public function __construct(string $accessKeyId, string $secretKey, string $sessionToken, string $region = 'us-east-1', string $endpoint = null, bool $https = true, int $port = null)
+    public static function fromEnvironmentVariables(string $region): self
+    {
+        return new self(
+            $_SERVER['AWS_ACCESS_KEY_ID'],
+            $_SERVER['AWS_SECRET_ACCESS_KEY'],
+            $_SERVER['AWS_SESSION_TOKEN'],
+            $region,
+        );
+    }
+
+    public function __construct(string $accessKeyId, string $secretKey, ?string $sessionToken, string $region, string $endpoint = null)
     {
         $this->accessKeyId = $accessKeyId;
         $this->secretKey = $secretKey;
@@ -26,22 +38,64 @@ class SimpleS3
         $this->endpoint = $endpoint;
     }
 
-    public function get(
-        string $bucketName,
-        string $objectKey
-    ): array {
-        $httpVerb = 'GET';
-        $uriPath = str_replace('%2F', '/', rawurlencode($objectKey));
+    /**
+     * @param Array<string, string> $headers
+     * @return Response
+     * @throws RuntimeException If the request failed.
+     */
+    public function get(string $bucket, string $key, array $headers = []): array
+    {
+        return $this->s3Request('GET', $bucket, $key, $headers);
+    }
+
+    /**
+     * `get()` will throw if the object doesn't exist.
+     * This method will return a 404 status and not throw instead.
+     *
+     * @param Array<string, string> $headers
+     * @return Response
+     * @throws RuntimeException If the request failed.
+     */
+    public function getIfExists(string $bucket, string $key, array $headers = []): array
+    {
+        return $this->s3Request('GET', $bucket, $key, $headers, '', false);
+    }
+
+    /**
+     * @param Array<string, string> $headers
+     * @return Response
+     * @throws RuntimeException If the request failed.
+     */
+    public function put(string $bucket, string $key, string $content, array $headers = []): array
+    {
+        return $this->s3Request('PUT', $bucket, $key, $headers, $content);
+    }
+
+    /**
+     * @param Array<string, string> $headers
+     * @return Response
+     * @throws RuntimeException If the request failed.
+     */
+    public function delete(string $bucket, string $key, array $headers = []): array
+    {
+        return $this->s3Request('DELETE', $bucket, $key, $headers);
+    }
+
+    /**
+     * @param Array<string, string> $headers
+     * @return Response
+     * @throws RuntimeException If the request failed.
+     */
+    private function s3Request(string $httpVerb, string $bucket, string $key, array $headers, string $body = '', $throwOn404 = true): array
+    {
+        $uriPath = str_replace('%2F', '/', rawurlencode($key));
         $uriPath = '/' . ltrim($uriPath, '/');
-        $body = '';
         $queryString = '';
-        $hostname = $this->getHostname($bucketName);
-        $headers = [
-            'host' => $hostname,
-        ];
+        $hostname = $this->getHostname($bucket);
+        $headers['host'] = $hostname;
 
         // Sign the request via headers
-        $headers = array_merge($headers, $this->signRequest($httpVerb, $uriPath, $queryString, $headers, $body));
+        $headers = $this->signRequest($httpVerb, $uriPath, $queryString, $headers, $body);
 
         if ($this->endpoint) {
             $url = $this->endpoint;
@@ -50,27 +104,32 @@ class SimpleS3
         }
         $url = "$url{$uriPath}?$queryString";
 
-        [$status, $response] = $this->curlRequest($httpVerb, $url, $headers);
+        [$status, $body, $responseHeaders] = $this->curlRequest($httpVerb, $url, $headers, $body);
 
-        $dom = new DOMDocument();
-        if (! $dom->loadXML($response)) {
-            throw new RuntimeException('Could not parse the AWS S3 response: ' . $response);
-        }
-        $errorMessage = '';
-        if ($dom->childNodes->item(0)->nodeName === 'Error') {
-            $errorMessage = $dom->childNodes->item(0)->textContent;
-        }
-        if ($status < 200 || $status >= 300) {
+        $shouldThrow404 = $throwOn404 && ($status === 404);
+        if ($shouldThrow404 || $status < 200 || ($status >= 400 && $status !== 404)) {
+            $errorMessage = '';
+            if ($body) {
+                $dom = new DOMDocument();
+                if (! $dom->loadXML($body)) {
+                    throw new RuntimeException('Could not parse the AWS S3 response: ' . $body);
+                }
+                if ($dom->childNodes->item(0)->nodeName === 'Error') {
+                    $errorMessage = $dom->childNodes->item(0)->textContent;
+                }
+            }
             throw $this->httpError($status, $errorMessage);
         }
 
-        return [$status, $response];
+        return [$status, $body, $responseHeaders];
     }
 
     /**
      * @param Array<string, string> $headers
+     * @return Response
+     * @throws RuntimeException If the request failed.
      */
-    private function curlRequest(string $httpVerb, string $url, array $headers): array
+    private function curlRequest(string $httpVerb, string $url, array $headers, string $body): array
     {
         $curlHeaders = [];
         foreach ($headers as $name => $value) {
@@ -78,25 +137,43 @@ class SimpleS3
         }
 
         $ch = curl_init($url);
+        $responseHeadersAsString = '';
         curl_setopt_array($ch, [
             CURLOPT_CUSTOMREQUEST => $httpVerb,
             CURLOPT_HTTPHEADER => $curlHeaders,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_TIMEOUT => 2,
+            CURLOPT_TIMEOUT => self::TIMEOUT_IN_SECONDS,
+            CURLOPT_POSTFIELDS => $body,
+            // So that `curl_exec` returns the response body
             CURLOPT_RETURNTRANSFER => true,
+            // Retrieve the response headers
+            CURLOPT_HEADERFUNCTION => function ($c, $data) use (&$responseHeadersAsString) {
+                $responseHeadersAsString .= $data;
+                return strlen($data);
+            },
         ]);
-        $response = curl_exec($ch);
+        $responseBody = curl_exec($ch);
         $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
         $success = $status !== 0 && $status !== 100 && $status !== 500 && $status !== 502 && $status !== 503;
-        if ($response === false || ! $success || curl_errno($ch) > 0) {
+        if ($responseBody === false || ! $success || curl_errno($ch) > 0) {
             throw $this->httpError($status, curl_error($ch));
         }
 
-        return [$status, $response];
+        $responseHeaders = iconv_mime_decode_headers(
+            $responseHeadersAsString,
+            ICONV_MIME_DECODE_CONTINUE_ON_ERROR,
+            'UTF-8',
+        );
+
+        return [$status, $responseBody, $responseHeaders];
     }
 
+    /**
+     * @param array<string, string> $headers
+     * @return array<string, string> Modified headers
+     */
     private function signRequest(
         string $httpVerb,
         string $uriPath,
@@ -107,15 +184,25 @@ class SimpleS3
         $dateAsText = gmdate('Ymd');
         $timeAsText = gmdate('Ymd\THis\Z');
         $scope = "$dateAsText/{$this->region}/s3/aws4_request";
+        $bodySignature = hash('sha256', $body);
 
-        ksort($headers, SORT_STRING | SORT_FLAG_CASE);
-        $headerNamesAsString = implode("\n", array_map('strtolower', array_keys($headers)));
-        $headerString = '';
-        foreach ($headers as $key => $value) {
-            $headerString .= strtolower($key) . ':' . trim($value);
+        $headers['x-amz-date'] = $timeAsText;
+        $headers['x-amz-content-sha256'] = $bodySignature;
+        if ($this->sessionToken) {
+            $headers['x-amz-security-token'] = $this->sessionToken;
         }
 
-        $canonicalRequest = "$httpVerb\n$uriPath\n$queryString\n$headerString\n$headerNamesAsString\nUNSIGNED-PAYLOAD";
+        // Ensure the headers always have the same order to have a valid AWS signature
+        $headers = $this->sortHeadersByName($headers);
+
+        // https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html
+        $headerNamesAsString = implode(";", array_map('strtolower', array_keys($headers)));
+        $headerString = '';
+        foreach ($headers as $key => $value) {
+            $headerString .= strtolower($key) . ':' . trim($value) . "\n";
+        }
+
+        $canonicalRequest = "$httpVerb\n$uriPath\n$queryString\n$headerString\n$headerNamesAsString\n$bodySignature";
 
         $stringToSign = "AWS4-HMAC-SHA256\n$timeAsText\n$scope\n" . hash('sha256', $canonicalRequest);
         $signingKey = hash_hmac('sha256', 'aws4_request',
@@ -128,14 +215,12 @@ class SimpleS3
         );
         $signature = hash_hmac('sha256', $stringToSign, $signingKey);
 
-        return [
-            'x-amz-content-sha256' => hash('sha256', $body),
-            'x-amz-date' => $timeAsText,
-            'authorization' => "AWS4-HMAC-SHA256 Credential={$this->accessKeyId}/$scope,SignedHeaders=$headerNamesAsString,Signature=$signature",
-        ];
+        $headers['authorization'] = "AWS4-HMAC-SHA256 Credential={$this->accessKeyId}/$scope,SignedHeaders=$headerNamesAsString,Signature=$signature";
+
+        return $headers;
     }
 
-    private function getHostname(string $bucketName): ?string
+    private function getHostname(string $bucketName): string
     {
         if ($this->region === 'us-east-1') return "$bucketName.s3.amazonaws.com";
         return "$bucketName.s3-{$this->region}.amazonaws.com";
@@ -144,5 +229,15 @@ class SimpleS3
     private function httpError(?int $status, ?string $message): RuntimeException
     {
         return new RuntimeException("AWS S3 request failed: $status $message");
+    }
+
+    /**
+     * @param Array<string, string> $headers
+     * @return Array<string, string>
+     */
+    private function sortHeadersByName(array $headers): array
+    {
+        ksort($headers, SORT_STRING | SORT_FLAG_CASE);
+        return $headers;
     }
 }
--- tests/SimpleS3Test.php
+++ tests/SimpleS3Test.php
@@ -4,11 +4,20 @@ namespace Mnapoli\SimpleS3\Test;
 
 use Mnapoli\SimpleS3\SimpleS3;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 class SimpleS3Test extends TestCase
 {
-    private static Process $s3ServerProcess;
+    private static ?Process $s3ServerProcess;
+    private SimpleS3 $s3;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        // Connect to our fake S3 server running locally
+        $this->s3 = new SimpleS3('S3RVER', 'S3RVER', '', 'us-east-1', 'http://localhost:4568');
+    }
 
     public function test with wrong access keys(): void
     {
@@ -19,17 +28,70 @@ class SimpleS3Test extends TestCase
 
     public function test get unknown bucket(): void
     {
-        $s3 = new SimpleS3('S3RVER', 'S3RVER', '', 'us-east-1', 'http://localhost:4568');
         $this->expectExceptionMessage('AWS S3 request failed: 404 NoSuchBucketThe specified bucket does not existbucket-name');
-        $s3->get('bucket-name', 'key');
+        $this->s3->get('bucket-name', 'key');
     }
 
     public function test get unknown object(): void
     {
-        $s3 = new SimpleS3('S3RVER', 'S3RVER', '', 'us-east-1', 'http://localhost:4568');
-        $this->expectExceptionMessage('AWS S3 request failed: 404 NoSuchKeyThe specified key does not exist.key');
-        [$status, $response] = $s3->get('my-bucket', 'key');
-        $this->assertEquals('200', $status);
+        $this->expectExceptionMessage('AWS S3 request failed: 404 NoSuchKeyThe specified key does not exist.unknown-key');
+        [$status] = $this->s3->get('my-bucket', 'unknown-key');
+        $this->assertEquals(200, $status);
+    }
+
+    public function test put(): void
+    {
+        [$status] = $this->s3->put('my-bucket', 'test-key', 'foo bar');
+        $this->assertEquals(200, $status);
+    }
+
+    /**
+     * @depends test put
+     */
+    public function test get(): void
+    {
+        [$status, $body, $headers] = $this->s3->get('my-bucket', 'test-key');
+        $this->assertEquals(200, $status);
+        $this->assertEquals('foo bar', $body);
+        $this->assertEquals('application/x-www-form-urlencoded', $headers['Content-Type']);
+        $this->assertEquals('"327b6f07435811239bc47e1544353273"', $headers['ETag']);
+    }
+
+
+    /**
+     * @depends test put
+     */
+    public function test get with headers(): void
+    {
+        [$status, $body] = $this->s3->get('my-bucket', 'test-key', [
+            'If-Modified-Since' => gmdate('D, d M Y H:i:s T'),
+        ]);
+        $this->assertEquals(304, $status);
+        $this->assertEquals('', $body);
+    }
+
+    /**
+     * @depends test put
+     */
+    public function test get if exists(): void
+    {
+        [$status, $body] = $this->s3->getIfExists('my-bucket', 'test-key');
+        $this->assertEquals(200, $status);
+        $this->assertEquals('foo bar', $body);
+
+        [$status] = $this->s3->getIfExists('my-bucket', 'unknown-key');
+        $this->assertEquals(404, $status);
+    }
+
+    /**
+     * @depends test put
+     */
+    public function test delete(): void
+    {
+        [$status] = $this->s3->delete('my-bucket', 'test-key');
+        $this->assertEquals(204, $status);
+        [$status] = $this->s3->getIfExists('my-bucket', 'test-key');
+        $this->assertEquals(404, $status);
     }
 
     public static function setUpBeforeClass(): void
@@ -46,6 +108,9 @@ class SimpleS3Test extends TestCase
 
     private static function startFakeS3Server(): void
     {
+        $fs = new Filesystem();
+        $fs->remove(__DIR__ . '/fixtures/my-bucket');
+
         $command = ['npm', 'run', 's3-server', '--', '--directory', __DIR__ . '/fixtures', '--configure-bucket', 'my-bucket'];
         self::$s3ServerProcess = new Process($command, __DIR__);
         self::$s3ServerProcess->start(function ($type, $output) {
